211d96dd1c057d65fc03665b7fd77072c64c0789
“weininghu1012” <weininghu1012@gmail.com>
2015-03-19 00:35:18 -0700

    data visualization for categorical data

---
+++ myattempt/vis-reg-ana.R
@@ -0,0 +1,34 @@
+#Visualization part for evaluating the data
+install.packages('plyr')
+library(plyr)
+library(ggplot2)
+setwd("/Users/apple/Documents/study/R/BikeSharingDemand/myattempt")
+train = read.csv("train.csv")
+attach(train)
+# http://www.cookbook-r.com/Manipulating_data/Summarizing_data/
+# We have numerical variables: temp, atemp, humidity and windspeed
+
+train$datetime = strptime(train$datetime, format = "%Y-%m-%d%H:%M:%S")
+train$weekday = weekdays(train$datetime)
+train$hour = train$datetime$hour
+# We have categorical variables: season(1/2/3/4), holiday(0/1), workingday(0/1),weather(1/2/3) and now we add hour and weekday
+season_count = aggregate(train["count"], by=train[c("season")], FUN=sum)
+holiday_count = aggregate(train["count"], by=train[c("holiday")], FUN=sum)
+workingday_count = aggregate(train["count"], by=train[c("workingday")], FUN=sum)
+weather_count = aggregate(train["count"], by=train[c("weather")], FUN=sum)
+hour_count = aggregate(train["count"], by=train[c("hour")], FUN=sum)
+weekday_count = aggregate(train["count"], by = train[c("weekday")], FUN = sum)
+
+season_count
+par("mar")
+par(mar=c(0.5,1,1,1))
+par(mfrow=c(3,2))
+s_c_plot =barplot(season_count$count,names.arg = season_count$season,main = "count VS season",xlab = "season", ylab = "count")
+h_c_plot =barplot(holiday_count$count,names.arg = holiday_count$holiday,main = "count VS holiday",xlab = "holiday", ylab = "count")
+wk_c_plot = barplot(weekday_count$count,names.arg = weekday_count$weekday,main = "count VS weekday",xlab = "weekday", ylab = "count")
+we_c_plot = barplot(weather_count$count,names.arg = weather_count$weather,main = "count VS weather",xlab = "weather", ylab = "count")
+h_c_plot = barplot(hour_count$count,names.arg = hour_count$hour,main = "count VS hour",xlab = "hour", ylab = "count")
+work_c_plot = barplot(workingday_count$count,names.arg = workingday_count$workingday,main = "count VS workingday",xlab = "workingday", ylab = "count")
+
+
+
