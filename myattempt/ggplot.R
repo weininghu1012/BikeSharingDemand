23b638e4e2dbd764ca2f549fbdb72a25cb4d8ac9
“weininghu1012” <weininghu1012@gmail.com>
2015-03-19 00:33:07 -0700

    add some plot to see trend versus categorical variables

--- myattempt/ggplot.R
+++
@@ -1,9 +0,0 @@
-#Creating plot to visualize the trends of borrowing time
-setwd("/Users/apple/Documents/study/R/BikeSharingDemand/myattempt")
-train <- read.csv("train.csv")
-train$datetime <- strptime(train$datetime, format = "%Y-%m-%d%H:%M:%S")
-train$weekday <- weekdays(train$datetime)
-train$hour <- train$datetime$hour
-#save average counts for each day/time in data frame
-day_hour_counts$hour <- as.data.frame(aggregate(train[,"count"],list(train$weekday, train$hour)),mean)
-?aggregate()
